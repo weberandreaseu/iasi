5dddc58c0bdf9967eb1dcdfd1ad7057d910f19c2
Andreas Weber <info@weberandreas.eu>
2019-02-20 13:47:11 +0100

    Add eigen composition test

--- iasi/composition.py
+++ iasi/composition.py
@@ -7,3 +7,17 @@ from luigi.util import requires
 @requires(MoveVariables)
 class Compositon(CopyNetcdfFile):
     pass
+
+
+class SingularValueComposition:
+
+    def __init__(self, group: Group):
+        # assert group contains U, s, VH
+        pass
+
+
+class EigenCompositon:
+    
+    def __init__(self, group: Group):
+        # assert group contains Q, s
+        pass
--- test/test_composition.py
+++ test/test_composition.py
@@ -1,10 +1,51 @@
+import logging
+import os
+import sys
 import unittest
+
+from netCDF4 import Dataset, Variable, Group
+
 import luigi
-from iasi import Compositon
+
+from iasi import Compositon, GroupCompression
+from iasi.composition import SingularValueComposition, EigenCompositon
+
+# TODO project wide logging configuration
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+# logger.basicConfig(level=logging.DEBUG)
+logger.setLevel(logging.DEBUG)
 
 
 class TestComposition(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # make sure there is a compressed file for testing purpose
+        cls.task = GroupCompression(
+            file='test/resources/MOTIV-single-event.nc',
+            dst='/tmp/iasi'
+            # force=True
+        )
+
+    def test_compression_output_exists(self):
+        path = self.task.output().path
+        logger.debug('Compression output path = %s', path)
+        self.assertTrue(os.path.isfile(path))
+
+    def test_singular_value_composition(self):
+        pass
+
+    def test_eigen_composition(self):
+        with Dataset(self.task.output().path) as nc:
+            atm_n = nc['/state/WV/atm_n']
+            self.assertIsInstance(atm_n, Group)
+            eig = EigenCompositon(atm_n)
+
+    @unittest.skip
     def test_group_compression(self):
+
         task = Compositon(
             file='test/resources/MOTIV-single-event.nc',
             dst='/tmp/iasi',
