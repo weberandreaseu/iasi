75f99f8f9287f6c60278aab67c61cdbcc98a3dd5
Andreas Weber <info@weberandreas.eu>
2019-03-05 20:21:39 +0100

    Add integration test

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,5 +1,25 @@
 import unittest
+from test_precision import TestCompareDecompressionResult
+from iasi import DecompressDataset, MoveVariables
+import luigi
+from netCDF4 import Dataset
 
-class IntegrationTest(unittest.TestCase):
-    def test_running_luigi(self):
-        self.assertTrue(True)
+
+class IntegrationTest(TestCompareDecompressionResult):
+    @classmethod
+    def setUpClass(cls):
+        file = 'data/input/METOPA_20160625001453_50240_20190209151722.nc'
+        # make sure there is a compressed file for testing purpose
+        compression = DecompressDataset(
+            file=file,
+            dst='/tmp/iasi',
+            force=True
+        )
+        uncompressed = MoveVariables(
+            file=file,
+            dst='/tmp/iasi',
+            force=True
+        )
+        assert luigi.build([compression, uncompressed], local_scheduler=True)
+        cls.compressed = Dataset(compression.output().path)
+        cls.uncompressed = Dataset(uncompressed.output().path)
