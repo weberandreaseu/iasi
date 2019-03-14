29df30ddb97df8cf6610277230678779d01a6ae8
Andreas Weber <info@weberandreas.eu>
2019-03-14 21:13:33 +0100

    Add class for covariance error

---
+++ iasi/metrics.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+class Covariance:
+    def __init__(self, nol: int, alt: np.ma.MaskedArray):
+        self.nol = nol
+        self.alt = alt
+
+    def gaussian(self, x, mu, sig):
+        """Gaussian function
+
+        :param x:   Input value
+        :param mu:  Mean value of gaussian
+        :param sig: Standard deviation of gaussian
+        """
+        return np.exp(-((x - mu)*(x - mu))/(2 * sig * sig))
+
+    def traf(self):
+        return np.block([
+            [np.diag(np.full(self.nol, +0.5)),
+             np.diag(np.full(self.nol, 0.5))],
+            [np.diag(np.full(self.nol, -1.0)), np.diag(np.full(self.nol, 1.0))]
+        ])
+
+    def s_atm_traf(self):
+        result = np.zeros((2 * self.nol, 2 * self.nol))
+        for i in range(self.nol):
+            for j in range(self.nol):
+                result[i, j] = self.gaussian(self.alt[i], self.alt[j], 2500)
+                result[i + self.nol, j + self.nol] = 0.01 * \
+                    self.gaussian(self.alt[i], self.alt[j], 2500)
+        return result
+
+    def s_atm(self):
+        traf = self.traf()
+        return np.linalg.inv(traf) @ self.s_atm_traf() @ np.linalg.inv(traf.T)
+
+    def error(self, original, compare):
+        return (original - compare) @ self.s_atm() @ (original - compare).T
--- notebooks/error.py
+++ notebooks/error.py
@@ -1,4 +1,5 @@
 # %%
+from iasi.metrics import Covariance
 import matplotlib.pyplot as plt
 import numpy as np
 from netCDF4 import Dataset
@@ -10,7 +11,7 @@ from iasi.quadrant import AssembleFourQuadrants
 # {0.5*(ln[H2O]+ln[HDO]),  ln[HDO]-ln[H2O]} Koordinatensystem.
 
 event = 0
-nc_rc = Dataset('/tmp/iasi/compression/MOTIV-single-event.nc')
+nc_rc = Dataset('/tmp/iasi/compression/0.001/MOTIV-single-event.nc')
 nc = Dataset('/tmp/iasi/groups/MOTIV-single-event.nc')
 nol = nc['atm_nol'][...]
 alt = nc['atm_altitude'][...]
@@ -23,36 +24,10 @@ avk = AssembleFourQuadrants(None).transform(avk[event], nol.data[event])
 
 level = nol.data[event]
 alt = alt.data[event]
-# class Error:
-traf = np.block([
-    [np.diag(np.full(level, +0.5)), np.diag(np.full(level, 0.5))],
-    [np.diag(np.full(level, -1.0)), np.diag(np.full(level, 1.0))]
-])
 
-def gaussian(x, mu, sig):
-    """Gaussian function
-
-    :param x:   Input value
-    :param mu:  Mean value of gaussian
-    :param sig: Standard deviation of gaussian
-    """
-    return np.exp(-((x - mu)*(x - mu))/(2 * sig * sig))
-
-
-s_atm_traf = np.zeros((2 * level, 2 * level))
-for i in range(level):
-    for j in range(level):
-        s_atm_traf[i, j] = gaussian(alt[i], alt[j], 2500)
-        s_atm_traf[i + level, j + level] = 0.01 * \
-            gaussian(alt[i], alt[j], 2500)
-
-# s_atm = s_cov
-# traf * s_atm * traf = s_atm_traf
-# s_atm = inv(traf) * s_atm_traf * inv(traf)
-s_atm = np.linalg.inv(traf) @ s_atm_traf @ np.linalg.inv(traf.T)
-s_err = (avk - np.eye(level * 2)) @ s_atm @ (avk - np.eye(level * 2)).T
-s_diff = (avk - avk_rc) @ s_atm @ (avk - avk_rc).T
-
+cov = Covariance(level, alt)
+s_err = cov.error(avk, np.eye(2*level))
+s_diff = cov.error(avk, avk_rc)
 
 plt.imshow(avk)
 plt.colorbar()
@@ -79,5 +54,3 @@ np.diag(s_diff)
 plt.plot(np.diag(s_diff))
 plt.title('S diff')
 plt.show()
-
-
