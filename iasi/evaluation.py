ba6e31b697804978fd9ce770ecdddbbf667c3448
Andreas Weber <info@weberandreas.eu>
2019-03-17 19:34:53 +0100

    Add aposteriori diff

--- iasi/evaluation.py
+++ iasi/evaluation.py
@@ -52,6 +52,7 @@ class EvaluateCompression(CustomTask):
         original.close()
         wv_error = WaterVapourError(nol, alt)
         error = wv_error.smoothing_error_all_measurements(avk)
+        a_error = wv_error.smoothing_error_aposteriori(avk)
         for task, input in zip(self.requires()['single_variable'], self.input()['single_variable']):
             nc = Dataset(input.path)
             if isinstance(nc['state/WV/atm_avk'], Variable):
@@ -62,6 +63,8 @@ class EvaluateCompression(CustomTask):
                     nc['state/WV/atm_avk']).reconstruct(nol)
             diff = wv_error.smoothing_error_all_measurements(
                 avk, avk_rc=avk_rc)
+            a_diff = wv_error.smoothing_error_aposteriori(
+                avk, avk_rc=avk_rc)
             df = df.append({
                 'variable': task.variable,
                 'compressed': task.compressed,
@@ -70,9 +73,15 @@ class EvaluateCompression(CustomTask):
                 'err_min': error[0],
                 'err_mean': error[1],
                 'err_max': error[2],
+                'err_apost_min': a_error[0],
+                'err_apost_mean': a_error[1],
+                'err_apost_max': a_error[2],
                 'diff_min': diff[0],
                 'diff_mean': diff[1],
-                'diff_max': diff[2]
+                'diff_max': diff[2],
+                'diff_apost_min': a_diff[0],
+                'diff_apost_mean': a_diff[1],
+                'diff_apost_max': a_diff[2]
                 # 'file': task.file,
             }, ignore_index=True)
             nc.close()
@@ -111,5 +120,33 @@ class WaterVapourError:
             err_max = max(err_max, s_err[level_of_interest, level_of_interest])
             err_min = min(err_min, s_err[level_of_interest, level_of_interest])
             err_mean += s_err[level_of_interest, level_of_interest]
-            n = n+1
+            n += 1
+        return (err_min, err_mean / n, err_max)
+
+    # TODO refactor
+    def smoothing_error_aposteriori(self, avk, avk_rc=None, level_of_interest=5):
+        err_max = -np.inf
+        err_min = np.inf
+        err_mean = 0
+        n = 0
+        for event in range(self.nol.shape[0]):
+            if np.ma.is_masked(self.nol[event]) or self.nol.data[event] > 29:
+                continue
+            l = self.nol.data[event]
+            # if reconstructed kernel not given create identity matrix and calculate covariance error
+            # else use reconstructed kernel to calculate covariance difference
+            cov = Covariance(l, self.alt[event])
+            if avk_rc is None:
+                Arc__ = np.identity(2*l)
+            else:
+                avk_rc_event = AssembleFourQuadrants().transform(avk_rc[event], l)
+                # A''rc
+                Arc__ = cov.posteriori_traf(avk_rc_event)
+            avk_event = AssembleFourQuadrants().transform(avk[event], l)
+            A__ = cov.posteriori_traf(avk_event)
+            s_err = cov.smoothing_error_covariance(A__, Arc__)
+            err_max = max(err_max, s_err[level_of_interest, level_of_interest])
+            err_min = min(err_min, s_err[level_of_interest, level_of_interest])
+            err_mean += s_err[level_of_interest, level_of_interest]
+            n += 1
         return (err_min, err_mean / n, err_max)
