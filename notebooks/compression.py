6d42412118a606638d09d1a1424c34690a420376
Andreas Weber <info@weberandreas.eu>
2019-03-17 19:37:44 +0100

    Evaluate compression

--- notebooks/compression.py
+++ notebooks/compression.py
@@ -1,69 +1,30 @@
-# %%[markdown]
-# # NetCDF Compression Performance
-#
-# Matrices containing significant amount of fill values.
-# To reduce size, we evaluate compression in netCDF4
-#
-
+# %%
+import luigi
+import matplotlib.pyplot as plt
 import numpy as np
-from timeit import default_timer as timer
-from netCDF4 import Dataset
 import pandas as pd
-import os
-import tempfile
 
-dimensions = (10000, 28, 28)
+from iasi.evaluation import EvaluateCompression
 
+task = EvaluateCompression(
+    # force=True,
+    dst='/tmp/iasi',
+    file='data/input/MOTIV-slice-1000.nc',
+    # file='test/resources/MOTIV-single-event.nc',
+    variable='state/WV/atm_avk'
+)
 
-def create_file(file: str = None, zlib=False):
-    if not os.path.exists('/tmp/compression'):
-        os.mkdir('/tmp/compression')
-    path = os.path.join('/tmp/compression', file)
-    nc = Dataset(path, 'w', format='NETCDF4')
-    nc.createDimension('event', dimensions[0])
-    nc.createDimension('grid_level', dimensions[1])
-    nc.createVariable('dummy', 'f8',
-                      ('event', 'grid_level', 'grid_level'),
-                      zlib=zlib)
-    return path, nc
+assert luigi.build([task], local_scheduler=True)
 
+df = pd.read_csv(task.output().open('r'), dtype={'compressed': bool})
+df.head()
 
-def write_values(nc: Dataset, array: np.ndarray):
-    slices = tuple(map(slice, array.shape))
-    nc['dummy'][slices] = array
+# %%
+df.plot.bar(x='threshold', y='size', rot=0)
+plt.title('Data size reduction')
+plt.show()
 
-
-# sequential read is an bad idea combined with compression
-# each read triggers decompression of complete variable
-def sequential_read(nc: Dataset):
-    events = nc.dimensions['event'].size
-    for event in range(events):
-        nc['dummy'][event]
-
-
-samples = [
-    {'file': 'full_plain.nc', 'zlib': False, 'dim': (10000, 28, 28)},
-    {'file': 'full_zlib.nc', 'zlib': True, 'dim': (10000, 28, 28)},
-    {'file': 'half_plain.nc', 'zlib': False, 'dim': (10000, 14, 28)},
-    {'file': 'half_zlib.nc', 'zlib': True, 'dim': (10000, 14, 28)}
-]
-
-for arg in samples:
-    file, nc = create_file(arg['file'], arg['zlib'])
-    values = np.random.uniform(size=arg['dim'])
-    t1 = timer()
-    write_values(nc, values)
-    t2 = timer()
-    # read values
-    nc['dummy'][...]
-    t3 = timer()
-    # sequential_read(nc)
-    t4 = timer()
-    arg['write_dur'] = t2 - t1
-    arg['read_full'] = t3 - t2
-    arg['read_seq'] = t4 - t3
-    arg['size'] = os.path.getsize(file)
-    nc.close()
-
-result = pd.DataFrame(samples)
-print(result)
+# %%
+df.plot.bar(x='threshold', y=['diff_min', 'diff_mean', 'diff_max',
+                              'diff_apost_min', 'diff_apost_mean', 'diff_apost_max'], logy=True, rot=0)
+plt.show()
