3529339461a8e66f9530556ea01a93f82807a7c6
Andreas Weber <info@weberandreas.eu>
2019-04-02 10:08:52 +0200

    Addjust plot for error estimation

--- notebooks/error.py
+++ notebooks/error.py
@@ -1,3 +1,6 @@
+# %% [markdown]
+# # Error Estimation
+
 # %%
 import matplotlib
 import luigi
@@ -21,28 +24,31 @@ assert luigi.build([task], local_scheduler=True)
 types = {'event': np.int, 'level_of_interest': np.int, 'err': np.float,
          type: np.int, 'rc_error': bool, 'threshold': np.float, 'var': str}
 df = pd.read_csv(task.output()['WV'].path, dtype=types)
-df.head()
 
 
 def filter_by(df: pd.DataFrame, var: str, level_of_interest: int, rc_error=True):
     return df[(df['rc_error'] == rc_error) & (df['var'] == var) & (df['level_of_interest'] == level_of_interest)]
 
 
-# %%
-wv_avk_10 = filter_by(df, 'atm_avk', -10)
-ax = wv_avk_10.groupby('threshold')['err'].mean().plot.bar(logy=True)
-wv_avk_10_mean_error = filter_by(df, 'atm_avk', -10, False)['err'].mean()
-ax.set_ylim(top=wv_avk_10_mean_error + 0.5)
-ax.axhline(wv_avk_10_mean_error, color='red')
-ax.set_ylabel('File size in kB')
-ax.set_xlabel('Threshold for eigenvalue selection')
-plt.title(f'Data size reduction for {task.variables[0]}')
-plt.show()
+def plot_error_estimation_for(gas: str, var: str, level_of_interest: int):
+    filtered_events = filter_by(df, var, level_of_interest)
+    ax = filtered_events.groupby('threshold')[
+        'err'].mean().plot.bar(logy=True, rot=0)
+    mean_error = filter_by(df, var, level_of_interest, False)['err'].mean()
+    # set ylim to make line for cov error visible
+    ax.set_ylim(top=1)
+    ax.axhline(mean_error, color='red', label='Error')
+    ax.set_ylabel('Reconstruction error')
+    ax.set_xlabel('Threshold for eigenvalue selection')
+    plt.title(f'Error estimation for {gas} {var} at {level_of_interest}')
+    plt.show()
 
-# %%
-ax = df.plot.bar(x='threshold', y='size', rot=0, legend=False)
-# ax.legend(loc='upper left')
-ax.set_ylabel('File size in kB')
-ax.set_xlabel('Threshold for eigenvalue selection')
-plt.title(f'Data size reduction for {task.variables[0]}')
-plt.show()
+
+df.head()
+
+# %%[markdown]
+# ## Water Vapour
+#
+# ### Averaging Kernel
+# - Level of interest: -16
+plot_error_estimation_for('WV', 'atm_avk', -16)
