33e05cc18030dadb2f612a14249d4c6685b5a408
Andreas Weber <info@weberandreas.eu>
2019-04-15 19:25:04 +0200

    Add numpy environment variables

---
+++ script/environments.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# used to reduce numpy threads for task level parallelism
+
+NUM_THREADS=1
+
+export MKL_NUM_THREADS=$NUM_THREADS
+export NUMEXPR_NUM_THREADS=$NUM_THREADS
+export OMP_NUM_THREADS=$NUM_THREADS
