52d486d3718777dd84cd7a691bde9d26c746110a
Andreas Weber <info@weberandreas.eu>
2019-04-25 15:41:42 +0200

    Plot eigenvalue maps for two days

--- notebooks/map_k.py
+++ notebooks/map_k.py
@@ -4,16 +4,35 @@ import numpy as np
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import pandas as pd
+import glob
+
+columns = [
+    'lat',
+    'lon',
+    'state/WV/avk/k',
+    'state/WV/n/k',
+    'state/WV/Tatmxavk/k',
+    'state/GHG/avk/k',
+    'state/GHG/n/k',
+    'state/GHG/Tatmxavk/k',
+    'state/HNO3/avk/k',
+    'state/HNO3/n/k',
+    'state/HNO3/Tatmxavk/k',
+    'state/Tatm/n/k',
+    'state/Tatm/avk/k'
+]
 
 
-def import_dataset(file: str) -> pd.DataFrame:
-    columns = ['lat', 'lon', 'state/WV/avk/k']
-    df = pd.DataFrame()
-    with Dataset(file) as nc:
-        for column in columns:
-            var = nc[column][...]
-            # assert not var.mask.any()
-            df[column] = np.ma.filled(var)
+def import_dataset(file_pattern: str) -> pd.DataFrame:
+    frames = []
+    for file in glob.glob(file_pattern):
+        df = pd.DataFrame()
+        with Dataset(file) as nc:
+            for column in columns:
+                var = nc[column][...]
+                # assert not var.mask.any()
+                df[column] = np.ma.filled(var)
+        frames.append(df)
     # print('Events before filter: %d' % len(df))
 
     # # filter north/south area
@@ -21,30 +40,65 @@ def import_dataset(file: str) -> pd.DataFrame:
     # # filter west/east area
     # df = df[(df.lon > -60) & (df.lon < 45)]
     # print('Events after filter: %d' % len(df))
-    return df
+    return pd.concat(frames, ignore_index=True)
 
 
-eigenvalues = import_dataset(
-    'data/eigenvalues/METOPA_20160201001156_48180_20190323165817.nc')
+df = import_dataset('data/eigenvalues/METOP*.nc')
+filtered = df
+print(f'Size before filter: {len(filtered)}')
+for column in filtered.columns.tolist()[2:]:
+    print(column)
+    filtered = filtered[filtered[column] >= 0]
+print(f'Size after filter: {len(filtered)}')
+
+#%%
+
+filtered.drop(['lon', 'lat'], axis=1).boxplot(rot='vertical')
+plt.show()
+
 # %% [markdown]
-# ### Filter noise
+# ### Plots
 
-eigenvalues = eigenvalues[eigenvalues['state/WV/avk/k'] >= 0]
+
+def plot_eigenvalues_of(variable: str, df=df):
+    # filter eigenvalues
+    eigenvalues = df[df[variable] >= 0]
+    # create map
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    sc = ax.scatter(eigenvalues.lon,
+                    eigenvalues.lat,
+                    c=eigenvalues[variable],
+                    marker='.',
+                    s=2,
+                    transform=ccrs.PlateCarree())
+    plt.colorbar(sc)
+    ax.coastlines()
+    plt.show()
+
 
 # %%
-eigenvalues['state/WV/avk/k'].plot.box()
-plt.title('state/WV/avk/k')
-plt.show()
+plot_eigenvalues_of('state/WV/avk/k')
 
 # %%
-ax = plt.axes(projection=ccrs.PlateCarree())
-# ax.set_extent([-65, 50, 65, -20], crs=ccrs.PlateCarree())
+plot_eigenvalues_of('state/WV/n/k')
 
-sc = ax.scatter(eigenvalues.lon,
-                eigenvalues.lat,
-                c=eigenvalues['state/WV/avk/k'],
-                #    alpha=0.1,
-                transform=ccrs.PlateCarree())
-plt.colorbar(sc)
-ax.coastlines()
-plt.show()
+# %%
+plot_eigenvalues_of('state/WV/Tatmxavk/k')
+
+# %%
+plot_eigenvalues_of('state/GHG/avk/k')
+
+# %%
+plot_eigenvalues_of('state/GHG/n/k')
+
+# %%
+plot_eigenvalues_of('state/GHG/Tatmxavk/k')
+
+# %%
+plot_eigenvalues_of('state/HNO3/avk/k')
+
+# %%
+plot_eigenvalues_of('state/HNO3/n/k')
+
+# %%
+plot_eigenvalues_of('state/HNO3/Tatmxavk/k')
