bebca91881d9b3cb1c2e4002e53cf783a30871c1
Andreas Weber <info@weberandreas.eu>
2019-05-08 14:41:27 +0200

    Combine two plots in one figure

--- notebooks/map_k.py
+++ notebooks/map_k.py
@@ -43,15 +43,61 @@ def import_dataset(file_pattern: str) -> pd.DataFrame:
     return pd.concat(frames, ignore_index=True)
 
 
-df = import_dataset('data/eigenvalues/METOP*.nc')
-filtered = df
-print(f'Size before filter: {len(filtered)}')
-for column in filtered.columns.tolist()[2:]:
-    print(column)
-    filtered = filtered[filtered[column] >= 0]
-print(f'Size after filter: {len(filtered)}')
+eig_winter = import_dataset('data/eigenvalues/METOP*20160201*.nc')
+eig_summer = import_dataset('data/eigenvalues/METOP*20160801*.nc')
 
-#%%
+eig_winter = eig_winter[eig_winter['state/WV/avk/k'] > 0]
+eig_summer = eig_summer[eig_summer['state/WV/avk/k'] > 0]
+
+
+# print(f'Size before filter: {len(filtered)}')
+# for column in filtered.columns.tolist()[2:]:
+#     print(column)
+#     filtered = filtered[filtered[column] >= 0]
+# print(f'Size after filter: {len(filtered)}')
+# %%
+
+
+def plot_season_map(summer: pd.DataFrame, winter: pd.DataFrame, var):
+    min_k = min(summer[var].min(), winter[var].min())
+    max_k = max(summer[var].max(), winter[var].max())
+    fig, (ax1, ax2) = plt.subplots(
+        2, 1, figsize=(6, 6), subplot_kw={'projection': ccrs.PlateCarree()})
+    sc1 = ax1.scatter(
+        summer.lon,
+        summer.lat,
+        c=summer[var],
+        marker='.',
+        s=2,
+        # log scale for error
+        vmin=min_k,
+        vmax=max_k,
+        transform=ccrs.PlateCarree())
+    # plt.colorbar(sc1)
+    ax1.coastlines()
+    ax1.set_title('2016-08-01')
+    sc2 = ax2.scatter(
+        winter.lon,
+        winter.lat,
+        c=winter[var],
+        marker='.',
+        s=2,
+        # log scale for error
+        vmin=min_k,
+        vmax=max_k,
+        transform=ccrs.PlateCarree())
+    ax2.coastlines()
+    ax2.set_title('2016-02-01')
+    fig.subplots_adjust(right=0.82)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.02, 0.7])
+    fig.colorbar(sc2, cax=cbar_ax)
+    plt.savefig('wv_avk_rank_e-3.pdf', bbox_inches='tight')
+
+
+# %%
+plot_season_map(eig_summer, eig_winter, 'state/WV/avk/k')
+
+# %%
 
 filtered.drop(['lon', 'lat'], axis=1).boxplot(rot='vertical')
 plt.show()
