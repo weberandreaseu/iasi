a4f6da3cbb4567d0007ea16dec8664bbebf56a1e
Andreas Weber <info@weberandreas.eu>
2019-05-11 14:52:41 +0200

    Add geographic area

--- notebooks/cluster.py
+++ notebooks/cluster.py
@@ -6,6 +6,30 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from netCDF4 import Dataset
+from typing import Tuple, List
+
+Coordinate = Tuple[float, float]
+CoordinateRange = Tuple[float, float]
+
+
+class GeographicArea:
+    """
+    """
+
+    def __init__(self, lat: CoordinateRange, lon: CoordinateRange):
+        """Extend of area in lat lon
+
+        :param lat  : Tupel(north, south)
+        :param lon  : Tupel(west, east)
+        """
+        self.lat = lat
+        self.lon = lon
+
+    def filter_dataframe(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df[(df.lon.between(*self.lon)) & (df.lat.between(self.lat[1], self.lat[0]))]
+
+    def get_extend(self) -> List:
+        return [*self.lon, *self.lat]
 
 
 def import_dataset(file: str) -> pd.DataFrame:
@@ -20,18 +44,15 @@ def import_dataset(file: str) -> pd.DataFrame:
                 df[column] = np.log(var.data)
             else:
                 df[column] = var.data
-    print('Events before filter: %d' % len(df))
-
-    # filter north/south area
-    df = df[(df.lat < 60) & (df.lat > -15)]
-    # filter west/east area
-    df = df[(df.lon > -60) & (df.lon < 45)]
-    print('Events after filter: %d' % len(df))
     return df
 
 
 # %%
+area = GeographicArea(lat=(50, -25), lon=(-45, 60))
+
+# %%
 df = import_dataset('data/input/METOPAB_20160625_global_evening.nc')
+df = area.filter_dataframe(df)
 
 # %%
 y = df['delD']
@@ -44,7 +65,7 @@ plt.show()
 
 # %%
 ax = plt.axes(projection=ccrs.PlateCarree())
-ax.set_extent([-65, 50, 65, -20], crs=ccrs.PlateCarree())
+ax.set_extent(area.get_extend(), crs=ccrs.PlateCarree())
 ax.coastlines()
 ax.scatter(df.lon, df.lat, s=4)
 plt.show()
