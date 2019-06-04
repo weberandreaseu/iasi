7a2aa0994561131b36b6c631a4f602647a29dc72
Andreas Weber <info@weberandreas.eu>
2019-06-04 16:40:53 +0200

    Add test for complex eigenvectors

--- test/test_errors.py
+++ test/test_errors.py
@@ -13,3 +13,16 @@ class TestErrors(unittest.TestCase):
             dst='/tmp/iasi'
         )
         assert luigi.build([task], local_scheduler=True)
+
+    def test_complex_eigenvectors(self):
+        """From METOPB_20160112181458_17220_20190313182845.nc 
+
+        eigen decomposition of /state/WV/n:11780 has complex eigenvectors
+        """
+
+        task = CompressDataset(
+            file='test/resources/MOTIV-complex-eigenvecors.nc',
+            force_upstream=True,
+            dst='/tmp/iasi'
+        )
+        assert luigi.build([task], local_scheduler=True)
