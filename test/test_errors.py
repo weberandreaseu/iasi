4c535ad617469ab2a854fc29b453fac7131274bc
Andreas Weber <info@weberandreas.eu>
2019-06-04 11:36:17 +0200

    Add test for SVD converge

--- test/test_errors.py
+++ test/test_errors.py
@@ -1,12 +1,15 @@
+import luigi
 import unittest
-from netCDF4 import Dataset
-from iasi.decomposition import SingularValueDecomposition
-class TestErrors:
+from iasi import CompressDataset
+
+
+class TestErrors(unittest.TestCase):
     """Common errors that occur for some events"""
 
     def test_svd_converges(self):
-        nc = Dataset('test/resources/METOPB_20160709011752_19753_20190314031224.nc')
-        nol = 18
-        group = nc['/state/WV']
-        variable = nc['/state/WV/avk']
-        svd = SingularValueDecomposition()
+        task = CompressDataset(
+            file='test/resources/MOTIV-svd-did-not-converge.nc',
+            force_upstream=True,
+            dst='/tmp/iasi'
+        )
+        assert luigi.build([task], local_scheduler=True)
