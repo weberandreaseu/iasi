Fix assumed covariance for HNO3

iasi/sig.py
@@ -9,6 +9,7 @@ nol = 28
 alt = nc['atm_altitude'][0, :nol].data
 # altitude tropopause
 alt_trop = 9486.431
+# alt_trop = 3800
 # altitude stratosphere TODO end of stratosphere?
 alt_strat = 25000
 
@@ -52,20 +53,26 @@ a_HNO3 = np.ndarray((nol))
 for i in range(nol):
 
     # surface is more than 4km below tropopause
-    # if alt[0] < alt_trop - 4000:
-    # TODO: template is strage here. ask for feedback
-    if alt[i] < alt_trop - 4000:
-        a_HNO3[i] = 2400 + (alt[i] - alt[0]) * \
-            ((1200 - 2400)/(alt_trop - 4000 - alt[0]))
-    if alt_trop - 4000 <= alt[i] < alt_trop + 8000:
-        a_HNO3[i] = 1200
-    if alt_trop + 8000 <= alt[i] < 50000:
-        a_HNO3[i] = 1200 + (alt[i] - (alt_trop + 8000)) * \
-            ((300-1200) / (50000 - (alt_trop + 8000)))
-    if alt[i] >= 50000:
-        a_HNO3[i] = 3000
-    # else:
-    #     pass
+    if alt[0] < alt_trop - 4000:
+        # TODO: template is strage here. ask for feedback
+        if alt[i] < alt_trop - 4000:
+            a_HNO3[i] = 2400 + (alt[i] - alt[0]) * \
+                ((1200 - 2400)/(alt_trop - 4000 - alt[0]))
+        if alt_trop - 4000 <= alt[i] < alt_trop + 8000:
+            a_HNO3[i] = 1200
+        if alt_trop + 8000 <= alt[i] < 50000:
+            a_HNO3[i] = 1200 + (alt[i] - (alt_trop + 8000)) * \
+                ((300-1200) / (50000 - (alt_trop + 8000)))
+        if alt[i] >= 50000:
+            a_HNO3[i] = 300
+    else:
+        if alt_trop - 4000 < alt[i] < alt_trop + 8000:
+            a_HNO3[i] = 1200
+        if alt_trop + 8000 < alt[i] < 50000:
+            a_HNO3[i] = 1200 + (alt[i] - (alt_trop + 8000)) * \
+                ((300 - 1200)/(50000 - (alt_trop + 8000)))
+        if alt[i] >= 50000:
+            a_HNO3[i] = 300
 
 sa_HNO3 = assumed_covariance(a_HNO3, sig * 1.2)
 sa_CH4 = assumed_covariance(a_CH4, sig * 0.6)
@@ -73,11 +80,12 @@ sa_CH4 = assumed_covariance(a_CH4, sig * 0.6)
 
 plt.imshow(sa_HNO3)
 plt.show()
+plt.plot(a_HNO3)
+plt.show()
 
 
 # %%
-# plt.plot(sig)
-plt.plot(a_HNO3)
+# plt.plot(a_HNO3)
 
 
 # %%