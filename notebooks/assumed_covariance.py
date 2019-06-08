df81392ee41ac62709438945d83bb6e8cc331727
Andreas Weber <info@weberandreas.eu>
2019-06-08 17:47:32 +0200

    Add projection method

--- notebooks/assumed_covariance.py
+++ notebooks/assumed_covariance.py
@@ -23,23 +23,34 @@ def default_color(i: int):
     return plt.rcParams['axes.prop_cycle'].by_key()['color'][i]
 
 
-def plot_std_and_amp(sig, amp, text_pos=1):
+def project_alt_to_index(alt_ref) -> float:
+    alt_prev = alt[0]
+    for i, alt_i in enumerate(alt):
+        if alt_i > alt_ref:
+            return i + (alt_ref - alt_prev) / (alt_i - alt_prev) - 1
+        else:
+            alt_prev = alt_i
+
+
+def plot_std_and_amp(sig, amp, text_pos=1, alt_strat=25000):
     fig, ax1 = plt.subplots(figsize=(4, 4))
     # stratosphere
-    ax1.axhline(25, color='gray', linestyle='dotted')
-    ax1.text(text_pos, 26, 'stratosphere',  color='gray')
+    alt_s = project_alt_to_index(alt_strat)
+    ax1.axhline(alt_s, color='gray', linestyle='dotted')
+    ax1.text(text_pos, alt_s + 0.6, 'stratosphere',  color='gray')
     # tropopause
-    ax1.axhline(alt_trop / 1000, color='gray', linestyle='dotted')
-    ax1.text(text_pos, (alt_trop / 1000) + 1, 'tropopause', color='gray')
+    alt_t = project_alt_to_index(alt_trop)
+    ax1.axhline(alt_t, color='gray', linestyle='dotted')
+    ax1.text(text_pos, (alt_t) + 0.6, 'tropopause', color='gray')
 
     # plot amplitudes (ax1)
     c_amp = default_color(0)
     if len(amp) == 2:
-        ax1.plot(amp[0], alt / 1000, color=c_amp, linestyle='-')
-        ax1.plot(amp[1], alt / 1000, color=c_amp, linestyle='-.')
+        ax1.plot(amp[0], np.arange(nol), color=c_amp, linestyle='-')
+        ax1.plot(amp[1], np.arange(nol), color=c_amp, linestyle='-.')
     else:
-        ax1.plot(amp, alt / 1000, color=c_amp, linestyle='-')
-    ax1.set_ylabel('altitude [km]')
+        ax1.plot(amp, np.arange(nol), color=c_amp, linestyle='-')
+    ax1.set_ylabel('altitude [level]')
     ax1.set_xlabel('amplitude')
     ax1.xaxis.label.set_color(c_amp)
     ax1.tick_params(axis='x', colors=c_amp)
@@ -47,8 +58,8 @@ def plot_std_and_amp(sig, amp, text_pos=1):
     # plot standard deviation (ax2)
     c_sig = default_color(1)
     ax2 = ax1.twiny()
-    ax2.plot(sigma, alt / 1000, color=c_sig)
-    ax2.set_xlabel('standard deviation')
+    ax2.plot(sigma / 1000, np.arange(nol), color=c_sig)
+    ax2.set_xlabel('standard deviation [km]')
     ax2.tick_params(axis='x', colors=c_sig)
     ax2.xaxis.label.set_color(c_sig)
     return ax1, ax2
@@ -74,8 +85,9 @@ plt.show()
 text_pos = 0.8
 ax1, ax2 = plot_std_and_amp(sigma, [amp_H2O, amp_delD], text_pos=text_pos)
 # 5km
-ax1.axhline(5, color='gray', linestyle='dotted')
-ax1.text(text_pos, 6, '5 km', color='gray')
+alt_5 = project_alt_to_index(5000)
+ax1.axhline(alt_5, color='gray', linestyle='dotted')
+ax1.text(text_pos, alt_5 + 0.6, '5 km', color='gray')
 plt.show()
 
 
@@ -89,7 +101,7 @@ plt.xlabel('level')
 plt.ylabel('level')
 plt.show()
 
-sig = ghg.sigma(0, 0.6)
+sig = ghg.sigma(0, f_sigma= 0.6)
 amp = ghg._amplitude(0)
 
 plot_std_and_amp(sig, amp, text_pos=0.18)
@@ -106,16 +118,16 @@ plt.show()
 sig = hno3.sigma(0, f_sigma=1.2)
 amp = hno3._amplitude(0)
 
-text_pos = 1.4
+text_pos = 1.3
 ax1, ax2 = plot_std_and_amp(sig, amp, text_pos=text_pos)
 
-tropo_4 = (alt_trop - 4000) / 1000
+tropo_4 = project_alt_to_index(alt_trop - 4000)
 ax1.axhline(tropo_4, color='gray', linestyle='dotted')
-ax1.text(text_pos, tropo_4 + 1, 'tropopause - 4 km', color='gray')
+ax1.text(text_pos, tropo_4 + 0.6, 'tropopause - 4 km', color='gray')
 
-tropo_8 = (alt_trop + 8000) / 1000
+tropo_8 = project_alt_to_index(alt_trop + 8000)
 ax1.axhline(tropo_8, color='gray', linestyle='dotted')
-ax1.text(text_pos, tropo_8 + 1, 'tropopause + 8 km', color='gray')
+ax1.text(text_pos, tropo_8 + 0.6, 'tropopause + 8 km', color='gray')
 
 plt.show()
 
@@ -128,16 +140,16 @@ plt.imshow(cov)
 plt.colorbar()
 plt.show()
 
-text_pos=1.5
+text_pos = 1.55
 amp, sig = tatm.assumed_covariance_temperature(0, return_tuple=True)
 ax1, ax2 = plot_std_and_amp(sig, amp, text_pos=text_pos)
 
-srf_4 = (alt[0] + 4000) / 1000
+srf_4 = project_alt_to_index(alt[0] + 4000)
 ax1.axhline(srf_4, color='gray', linestyle='dotted')
-ax1.text(text_pos, srf_4 + 1, 'surface + 4 km', color='gray')
+ax1.text(text_pos, srf_4 + 0.6, 'surface + 4 km', color='gray')
 
-tropo_5 = (alt_trop + 5000) / 1000
+tropo_5 = project_alt_to_index(alt_trop + 5000)
 ax1.axhline(tropo_5, color='gray', linestyle='dotted')
-ax1.text(text_pos, tropo_5 + 1, 'tropopause + 5 km', color='gray')
+ax1.text(text_pos, tropo_5 + 0.6, 'tropopause + 5 km', color='gray')
 
 plt.show()
